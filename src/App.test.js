57a029dc4e55e340928013771bcb55ee8a6af8d7
Brandon Bost <bbost9@uncc.edu>
2021-02-27 11:50:55 -0500

    minor update

--- src/App.test.js
+++ src/App.test.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Contact link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/Contact/i);
   expect(linkElement).toBeInTheDocument();
 });
 
-test('renders learn react link', () => {
+test('renders Contact link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/Contact/i);
   expect(linkElement).toBeInTheDocument();
