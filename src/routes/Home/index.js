7c04c596d033c94bb7afd1118b7813d9e0b020be
Brandon Bost <bbost9@uncc.edu>
2021-03-03 09:27:28 -0500

    added personal info to homepage

--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -10,7 +10,7 @@ class Home extends React.Component {
               Brandon Bost 
             </p>
             <p className="subtitle">
-              Software Engineer 
+              Software Engineering and Data Science Graduate 
             </p>
           </div>
         </section>
@@ -19,14 +19,25 @@ class Home extends React.Component {
           <div className="tile is-4 is-vertical is-parent">
             <div className="tile is-child box">
               <p className="title">About</p>
+              <p>
+                I am a recent graduate from the University of North Carolina at Charlotte.
+                I decided to build this site in an attempt to create foundations for a personal blog in which I can express
+                various interests, while also creating something tangable and functional for potential employers to review.
+                This site is built with ReactJS, using the Bulma css framework and EmailJS as a client-side email service. 
+                I am using GitHub's new actions feature for continuous integration to deploy to GitHub pages (as you can maybe tell
+                from the site address).
+              </p>
             </div>
-            <div className="tile is-child box">
+            {/* <div className="tile is-child box">
               <p className="title">Goals</p>
-            </div>
+            </div> */}
           </div>
           <div className="tile is-parent">
             <div className="tile is-child box">
               <p className="title">Projects</p>
+              <a href="https://github.com/Lyndogwin/salazar"><img src="https://gh-card.dev/repos/Lyndogwin/salazar.svg"/></a>
+              <a href="https://github.com/Lyndogwin/poli"><img src="https://gh-card.dev/repos/Lyndogwin/poli.svg"/></a>
+              <a href="https://github.com/Lyndogwin/portfolio"><img src="https://gh-card.dev/repos/Lyndogwin/portfolio.svg"/></a>
             </div>
           </div>
         </div>
