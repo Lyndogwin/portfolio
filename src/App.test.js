43a988dfb1aa163b1dba919a3660bf8c7de6ea4a
Brandon Bost <brandon.bost101@gmail.com>
2022-05-12 10:38:10 -0400

    staging to fix liquid error with github actions deployment

--- src/App.test.js
+++ src/App.test.js
@@ -8,8 +8,20 @@ test('renders Contact link', () => {
   expect(linkElement).toBeInTheDocument();
 });
 
-test('renders Contact link', () => {
+test('renders Home link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/Contact/i);
+  const linkElement = getByText(/Home/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Blog link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Blog/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders More link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/More/i);
   expect(linkElement).toBeInTheDocument();
 });
